bc9c63e4746c7a4f1ce5ae0ff131301360ffd465
raito <raitokomniak@gmail.com>
2023-04-06 13:03:13 +0300

    wrapped in scrollview

--- pennyplanner/lib/pages/history_page.dart
+++ pennyplanner/lib/pages/history_page.dart
@@ -75,160 +75,159 @@ class _HistoryPageState extends State<HistoryPage> {
     DateFormat dateFormat =
         DateFormat.yMMMMd(); // how you want it to be formatted
 
-    return Stack(children: [
-      Column(
-        children: [
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(children: [
-                  InkWell(
-                    child: Text(
-                      dateFormat.format(startDate!),
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w100,
-                          fontFamily: "Hind Siliguri",
-                          color: Color(0xff0F5B2E)),
-                    ),
-                    onTap: () async {
-                      // DateTime d = DateTime.now();
-
-                      startDate = await showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(), //get today's date
-                              firstDate: DateTime(
-                                  2000), //DateTime.now() - not to allow to choose before today.
-                              lastDate: DateTime(2101))
-                          .then((value) {
-                        setState(() {
-                          // d = value!;
-                        });
-                        return value;
-                      });
-                    },
-                  ),
-                  const Text(
-                    " - ",
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(children: [
+                InkWell(
+                  child: Text(
+                    dateFormat.format(startDate!),
                     textAlign: TextAlign.left,
-                    style: TextStyle(
+                    style: const TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.w100,
                         fontFamily: "Hind Siliguri",
                         color: Color(0xff0F5B2E)),
                   ),
-                  InkWell(
-                    onTap: () async {
-                      // DateTime d = DateTime.now();
+                  onTap: () async {
+                    // DateTime d = DateTime.now();
 
-                      endDate = await showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(), //get today's date
-                              firstDate: DateTime(
-                                  2000), //DateTime.now() - not to allow to choose before today.
-                              lastDate: DateTime(2101))
-                          .then((value) {
-                        setState(() {
-                          // d = value!;
-                        });
-                        return value;
+                    startDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(), //get today's date
+                            firstDate: DateTime(
+                                2000), //DateTime.now() - not to allow to choose before today.
+                            lastDate: DateTime(2101))
+                        .then((value) {
+                      setState(() {
+                        // d = value!;
                       });
-                    },
-                    child: Text(
-                      dateFormat.format(endDate!),
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w100,
-                          fontFamily: "Hind Siliguri",
-                          color: Color(0xff0F5B2E)),
+                      return value;
+                    });
+                  },
+                ),
+                const Text(
+                  " - ",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w100,
+                      fontFamily: "Hind Siliguri",
+                      color: Color(0xff0F5B2E)),
+                ),
+                InkWell(
+                  onTap: () async {
+                    // DateTime d = DateTime.now();
+
+                    endDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(), //get today's date
+                            firstDate: DateTime(
+                                2000), //DateTime.now() - not to allow to choose before today.
+                            lastDate: DateTime(2101))
+                        .then((value) {
+                      setState(() {
+                        // d = value!;
+                      });
+                      return value;
+                    });
+                  },
+                  child: Text(
+                    dateFormat.format(endDate!),
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w100,
+                        fontFamily: "Hind Siliguri",
+                        color: Color(0xff0F5B2E)),
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: PieChart(
+                  dataMap: calcPercentages(),
+                  animationDuration: const Duration(milliseconds: 800),
+                  chartLegendSpacing: 32,
+                  chartRadius: MediaQuery.of(context).size.width / 3.2,
+                  initialAngleInDegree: 0,
+                  chartType: ChartType.ring,
+                  ringStrokeWidth: 32,
+                  centerText: "",
+                  legendOptions: const LegendOptions(
+                    showLegendsInRow: false,
+                    legendPosition: LegendPosition.right,
+                    showLegends: true,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ]),
-              ),
-              Container(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: PieChart(
-                    dataMap: calcPercentages(),
-                    animationDuration: const Duration(milliseconds: 800),
-                    chartLegendSpacing: 32,
-                    chartRadius: MediaQuery.of(context).size.width / 3.2,
-                    initialAngleInDegree: 0,
-                    chartType: ChartType.ring,
-                    ringStrokeWidth: 32,
-                    centerText: "",
-                    legendOptions: const LegendOptions(
-                      showLegendsInRow: false,
-                      legendPosition: LegendPosition.right,
-                      showLegends: true,
-                      legendTextStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    chartValuesOptions: const ChartValuesOptions(
-                      showChartValueBackground: true,
-                      showChartValues: true,
-                      showChartValuesInPercentage: false,
-                      showChartValuesOutside: false,
-                      decimalPlaces: 1,
-                    ),
-                    // gradientList: ---To add gradient colors---
-                    // emptyColorGradient: ---Empty Color gradient---
-                  )),
-              for (int i = 0; i < resultsData.length; i++) ...[
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: true,
+                    showChartValues: true,
+                    showChartValuesInPercentage: false,
+                    showChartValuesOutside: false,
+                    decimalPlaces: 1,
                   ),
-                  elevation: 3,
-                  child: ExpandableTheme(
-                    data: const ExpandableThemeData(hasIcon: false),
-                    child: ExpandablePanel(
-                      header: Container(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          resultsData[i][0].toString(),
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w100,
-                              fontFamily: "Hind Siliguri",
-                              color: Color(0xff0F5B2E)),
-                        ),
+                  // gradientList: ---To add gradient colors---
+                  // emptyColorGradient: ---Empty Color gradient---
+                )),
+            for (int i = 0; i < resultsData.length; i++) ...[
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                elevation: 3,
+                child: ExpandableTheme(
+                  data: const ExpandableThemeData(hasIcon: false),
+                  child: ExpandablePanel(
+                    header: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        resultsData[i][0].toString(),
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w100,
+                            fontFamily: "Hind Siliguri",
+                            color: Color(0xff0F5B2E)),
                       ),
-                      collapsed: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SizedBox(
-                            height: 20,
-                            child: Text("${resultsData[i][1].length} results")),
-                      ),
-                      expanded: Column(children: [
-                        for (var e in resultsData[i][1])
-                          Row(children: [
-                            for (var ex in e)
-                              Container(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    ex.toString(),
-                                    textAlign: TextAlign.left,
-                                    style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w100,
-                                        fontFamily: "Hind Siliguri",
-                                        color: Color(0xff0F5B2E)),
-                                  ))
-                          ]),
-                      ]),
                     ),
+                    collapsed: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                          height: 20,
+                          child: Text("${resultsData[i][1].length} results")),
+                    ),
+                    expanded: Column(children: [
+                      for (var e in resultsData[i][1])
+                        Row(children: [
+                          for (var ex in e)
+                            Container(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  ex.toString(),
+                                  textAlign: TextAlign.left,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w100,
+                                      fontFamily: "Hind Siliguri",
+                                      color: Color(0xff0F5B2E)),
+                                ))
+                        ]),
+                    ]),
                   ),
-                )
-              ],
+                ),
+              )
             ],
-          ),
-        ],
-      )
-    ]);
+          ],
+        ),
+      ],
+    ));
   }
 }
